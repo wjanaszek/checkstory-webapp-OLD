c4de24ad61a2318a18ccb598de1bb2636fea22aa
wjanaszek <trash.wjanaszek@gmail.com>
2017-10-08 21:59:58 +0200

    Work on dashboard in progress: added confirm dialog for logout, some little improvements in dashboard component

--- src/app/myaccount/myaccount.component.ts
+++ src/app/myaccount/myaccount.component.ts
@@ -1,8 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { fadeInAnimation } from '../animations/fadeInAnimation';
 
 @Component({
   selector: 'app-myaccount',
-  templateUrl: './myaccount.component.html'
+  templateUrl: './myaccount.component.html',
+  animations: [ fadeInAnimation ],
+  host: { '[@fadeInAnimation]': '' }
 })
 export class MyAccountComponent implements OnInit {
 
