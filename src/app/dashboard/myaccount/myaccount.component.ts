0be39d0fd6e1e55ca35dec3f8faaf7d750650e1f
wjanaszek <trash.wjanaszek@gmail.com>
2017-10-20 22:47:34 +0200

    Little improvements - async validation in change password dialog

--- src/app/dashboard/myaccount/myaccount.component.ts
+++ src/app/dashboard/myaccount/myaccount.component.ts
@@ -72,10 +72,13 @@ export class ChangePasswordDialogComponent {
   }
 
   validateOldPassword(control: AbstractControl) {
-    // TODO biezacy user ze store (trzeba zrobic store)
+    // TODO biezacy user ze store (trzeba zrobic store) - tymczasowo z localStorage
     // return this.changePasswordValidationService.checkPassword(control.value).map(res => {
     //   return res ? null : { emailTaken: true };
     // });
-    return null;
+    return this.changePasswordValidationService.checkPassword(JSON.parse(localStorage.getItem('currentUser')).id, control.value)
+      .map(res => {
+        return res ? null : { passwordNotCorrect: true };
+      });
   }
 }
