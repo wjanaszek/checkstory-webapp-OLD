bc99b8a66a0fc199a28b45b254564b07fca9f339
wjanaszek <trash.wjanaszek@gmail.com>
2017-10-21 12:52:34 +0200

    Requestparam fix...

--- src/app/shared/services/stories.service.ts
+++ src/app/shared/services/stories.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Story } from '../models/story.model';
-import { Headers, Http } from '@angular/http';
+import { Headers, Http, URLSearchParams } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 import { environment } from '../../../environments/environment';
 
@@ -13,13 +13,16 @@ export class StoriesService {
     const currentUser = JSON.parse(localStorage.getItem('currentUser'));
     const headers = new Headers();
     headers.append('Content-Type', 'application/json');
-    headers.append('userId', currentUser.id);
-    return this.http.get(environment.apiUrl + '/api/stories', { headers: headers })
+    const requestParam = new URLSearchParams();
+    requestParam.append('userId', `${currentUser.id}`);
+    return this.http.get(environment.apiUrl + '/api/stories', { headers: headers, params: requestParam })
       .map(res => res.json());
   }
 
   getById(id: number): Observable<Story> {
-    return this.http.get(environment.apiUrl + `api/stories/${id}`)
+    const headers = new Headers();
+    headers.append('Content-Type', 'application/json');
+    return this.http.get(environment.apiUrl + `/api/stories/${id}`, { headers: headers })
       .map(res => res.json());
   }
 }
