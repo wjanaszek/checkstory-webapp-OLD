84221379fee5f21bc48d139d34e87b55d5acf138
wjanaszek <trash.wjanaszek@gmail.com>
2017-10-26 18:22:47 +0200

    Little fix 4

--- src/app/shared/services/stories.service.ts
+++ src/app/shared/services/stories.service.ts
@@ -41,6 +41,8 @@ export class StoriesService {
     const headers = new Headers();
     headers.append('Content-Type', 'application/json');
     console.log('story to update: ' + JSON.stringify(story));
+    const currentUser = JSON.parse(localStorage.getItem('currentUser'));
+    story.owner = currentUser;
     story.longitude = Number(story.longitude);
     console.log('story to update: ' + JSON.stringify(story));
     return this.http.put(environment.apiUrl + `/api/stories/${story.id}`, JSON.stringify(story), { headers: headers })
