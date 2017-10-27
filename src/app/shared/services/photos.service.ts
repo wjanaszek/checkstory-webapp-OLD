258f30bcb08149b98794d48e3743fc292f2f8ebc
wjanaszek <trash.wjanaszek@gmail.com>
2017-10-27 19:47:37 +0200

    Create method added

--- src/app/shared/services/photos.service.ts
+++ src/app/shared/services/photos.service.ts
@@ -9,13 +9,13 @@ export class PhotosService {
 
   constructor(private http: Http) { }
 
-  getAll(storyId: number): Observable<Photo[]> {
+  getAll(storyNumber: number): Observable<Photo[]> {
     const currentUser = JSON.parse(localStorage.getItem('currentUser'));
     const headers = new Headers();
     headers.append('Content-Type', 'application/json');
     const requestParam = new URLSearchParams();
     requestParam.append('userId', `${currentUser.id}`);
-    return this.http.get(environment.apiUrl + `/api/stories/${storyId}/photos`, { headers: headers, params: requestParam })
+    return this.http.get(environment.apiUrl + `/api/stories/${storyNumber}/photos`, { headers: headers, params: requestParam })
       .map(res => res.json());
   }
 
@@ -23,6 +23,13 @@ export class PhotosService {
     const headers = new Headers();
     headers.append('Content-Type', 'application/json');
     return this.http.get(environment.apiUrl + `/api/stories/${storyNumber}/photos/${photoNumber}`, headers)
+      .map(res => res.json());
+  }
+
+  create(photo: Photo) {
+    const headers = new Headers();
+    headers.append('Content-Type', 'application/json');
+    return this.http.post(environment.apiUrl + `/api/stories/${photo.storyNumber}/photos`, headers)
       .map(res => res.json());
   }
 
