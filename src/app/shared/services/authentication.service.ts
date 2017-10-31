b388c0b2e57f2c5d6f14313bf0f0331eab87d7b7
wjanaszek <trash.wjanaszek@gmail.com>
2017-10-31 23:29:11 +0100

    Working on jwt

--- src/app/shared/services/authentication.service.ts
+++ src/app/shared/services/authentication.service.ts
@@ -1,9 +1,8 @@
 import { Injectable } from '@angular/core';
-import { Http, Response } from '@angular/http';
+import { Http, Headers, Response } from '@angular/http';
 import 'rxjs/add/operator/map';
 import { Md5 } from 'ts-md5/dist/md5';
 import { environment } from '../../../environments/environment';
-import { jwt } from '../jwt.headers';
 
 @Injectable()
 export class AuthenticationService {
@@ -13,17 +12,28 @@ export class AuthenticationService {
   login(login: string, password: string) {
     const hashedPassword = Md5.hashStr(password);
     console.log('hashed password: ' + hashedPassword);
-    return this.http.post(environment.apiUrl + '/api/authenticate', JSON.stringify({ login: login, password: hashedPassword }), jwt())
-      .map((response: Response) => {
-      // login succesfull if there is a jwt token in response
-        const responseBody = response.json();
-        if (responseBody && responseBody.token) {
-          // store user details and jwt token in local storage to keep user logged in between page refreshes
-          localStorage.setItem('currentUser', JSON.stringify(responseBody.user));
-        }
+    const headers = new Headers({
+      'Content-Type': 'Application/json'});
+    return this.http.post(environment.apiUrl + '/login', JSON.stringify({ login: login, password: hashedPassword }), { headers: headers })
+      .map((res: Response) => {
+        const authorization = res.headers.get('Authorization');
+        console.log(authorization);
+        if (authorization !== null) {
 
-        return responseBody.user;
+        }
       });
+      // .map((response: Response) => {
+      //   console.log('headers: ' + JSON.stringify(response));
+      // // login succesfull if there is a jwt token in response
+      //   const responseBody = response.json();
+      //   console.log('succesfuly logged in ' + responseBody);
+      //   if (responseBody && responseBody.token) {
+      //     // store user details and jwt token in local storage to keep user logged in between page refreshes
+      //     localStorage.setItem('currentUser', JSON.stringify(responseBody.user));
+      //   }
+      //
+      //   return responseBody.user;
+      // });
   }
 
   logout() {
