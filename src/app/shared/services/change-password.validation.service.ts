6f1d2c1da9986dff20d268129db4055277a451ea
wjanaszek <trash.wjanaszek@gmail.com>
2017-11-01 18:52:55 +0100

    Change password validation service update

--- src/app/shared/services/change-password.validation.service.ts
+++ src/app/shared/services/change-password.validation.service.ts
@@ -1,16 +1,20 @@
 import { Injectable } from '@angular/core';
 import { environment } from '../../../environments/environment';
 import { Md5 } from 'ts-md5/dist/md5';
-import { AuthHttp } from 'angular2-jwt';
+import { AuthHttp, JwtHelper } from 'angular2-jwt';
 
 @Injectable()
 export class ChangePasswordValidationService {
 
+  jwtHelper: JwtHelper = new JwtHelper();
+
   constructor(private authHttp: AuthHttp) { }
 
-  checkPassword(id: number, password: string) {
+  checkPassword(password: string) {
     const hashedPassword = Md5.hashStr(password);
-    return this.authHttp.post(environment.apiUrl + `/api/users/checkPassword/${id}`, hashedPassword)
+    const token = localStorage.getItem('jwt-token');
+    const userId = this.jwtHelper.decodeToken(token).userId;
+    return this.authHttp.post(environment.apiUrl + `/api/users/checkPassword/${userId}`, hashedPassword)
       .map(res => res.json());
   }
 }
