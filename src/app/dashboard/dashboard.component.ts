b0843cab7bda3bfadd72da4abecc2cb680657806
wjanaszek <trash.wjanaszek@gmail.com>
2017-11-15 15:51:06 +0100

    New toolbar (global for whole app), little changes

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,8 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import { fadeInAnimation } from '../shared/animations/fadeInAnimation';
 import { Router } from '@angular/router';
 import { AuthenticationService } from '../shared/services/authentication.service';
 import { DialogsService } from '../shared/services/dialogs.service';
+import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MAT_DIALOG_DATA, MatDialog, MatDialogRef } from '@angular/material';
+import { ChangePasswordValidationService } from '../shared/services/change-password.validation.service';
+import { PasswordValidation } from '../shared/password.validation';
+import { ChangePasswordActionModel } from '../shared/models/change-password-action.model';
+import { User } from '../shared/models/user.model';
+import { UserService } from '../shared/services/user-service';
 
 @Component({
   selector: 'app-dashboard',
@@ -14,6 +21,8 @@ export class DashboardComponent implements OnInit {
   defaultRoute: boolean;
 
   constructor(private router: Router,
+              public changePasswordDialog: MatDialog,
+              private userService: UserService,
               private authenticationService: AuthenticationService,
               public dialogsService: DialogsService) { }
 
@@ -36,12 +45,57 @@ export class DashboardComponent implements OnInit {
   }
 
   openChangePasswordDialog() {
-    console.log('change password here?');
+    this.changePasswordDialog.open(ChangePasswordDialogComponent, {
+      disableClose: true,
+      data: this.changePassword.bind(this)
+    });
+  }
+
+  changePassword(event) {
+    const user = new User();
+    user.password = event.newPassword;
+    this.userService.update(user);
   }
 
   private goLogout() {
     this.authenticationService.logout();
     this.router.navigate(['/home']);
+  }
+}
+
+@Component({
+  selector: 'app-change-password',
+  templateUrl: './change.password.dialog.html'
+})
+export class ChangePasswordDialogComponent {
+
+  changePasswordForm: FormGroup;
+
+  constructor(@Inject(MAT_DIALOG_DATA) public data: any,
+              public dialogRef: MatDialogRef<ChangePasswordDialogComponent>,
+              private changePasswordValidationService: ChangePasswordValidationService,
+              private fb: FormBuilder) {
+    this.changePasswordForm = this.fb.group({
+      oldPassword: ['', [Validators.required], this.validateOldPassword.bind(this)],
+      newPassword: ['', Validators.required],
+      confirmPassword: ['', Validators.required]
+    }, {
+      validator: PasswordValidation.matchPasswordByChangePassword
+    });
+  }
+
+  changePassword() {
+    this.data(new ChangePasswordActionModel(
+      this.changePasswordForm.get('oldPassword').value,
+      this.changePasswordForm.get('newPassword').value)
+    );
+  }
+
+  validateOldPassword(control: AbstractControl) {
+    return this.changePasswordValidationService.checkPassword(control.value)
+      .map(res => {
+        return res ? null : { passwordNotCorrect: true };
+      });
   }
 }
 
