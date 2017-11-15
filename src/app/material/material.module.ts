ab1482a5cbfef108c7abe6ba2d3d396d97e90ff9
wjanaszek <trash.wjanaszek@gmail.com>
2017-11-15 14:35:42 +0100

    Fix because of new material module

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -3,7 +3,8 @@ import { CommonModule } from '@angular/common';
 import {
   MatButtonModule, MatCardModule, MatCheckboxModule, MatDatepickerModule, MatDialogModule, MatFormFieldModule,
   MatIconModule,
-  MatInputModule, MatListModule, MatMenuModule, MatNativeDateModule, MatProgressSpinnerModule, MatTabsModule,
+  MatInputModule, MatListModule, MatMenuModule, MatNativeDateModule, MatProgressSpinnerModule, MatSlideToggleModule,
+  MatTabsModule,
   MatToolbarModule, MatTooltipModule
 } from '@angular/material';
 
@@ -25,6 +26,25 @@ import {
     MatNativeDateModule,
     MatMenuModule,
     MatProgressSpinnerModule,
+    MatSlideToggleModule
+  ],
+  exports: [
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    MatButtonModule,
+    MatCardModule,
+    MatCheckboxModule,
+    MatTooltipModule,
+    MatListModule,
+    MatDialogModule,
+    MatToolbarModule,
+    MatTabsModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatMenuModule,
+    MatProgressSpinnerModule,
+    MatSlideToggleModule
   ],
   declarations: []
 })
