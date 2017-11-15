f00ad67650fbf2d46c21095be6cc9cee49f988e9
wjanaszek <trash.wjanaszek@gmail.com>
2017-11-15 20:48:14 +0100

    Working on improvement, to refresh after update etc

--- src/app/shared/services/message.service.ts
+++ src/app/shared/services/message.service.ts
@@ -2,20 +2,25 @@ import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs/Subject';
 import { Observable } from 'rxjs/Observable';
 
+export interface Message {
+  type: { 'itemCreated', 'itemUpdated', 'itemDeleted', 'info', 'loadAll' };
+  target: { 'photos', 'stories', 'photo', 'story' };
+}
+
 @Injectable()
 export class MessageService {
 
-  private subject: Subject<any>;
+  private subject: Subject<Message>;
 
   constructor() {
-    this.subject = new Subject<any>();
+    this.subject = new Subject<Message>();
   }
 
-  sendMessage(message: any) {
-    this.subject.next({ data: message });
+  sendMessage(message: Message) {
+    this.subject.next(message);
   }
 
-  getMessage(): Observable<any> {
+  getMessage(): Observable<Message> {
     return this.subject.asObservable();
   }
 }
